5e48dc95d3f0eec5a42e56a0ce28f24f7ab0546b
kajisha <kajisha@gmail.com>
2016-08-17 17:46:51 +0900

    delgate current_user method to parent_controller

--- app/cells/bbs/application_cell.rb
+++ app/cells/bbs/application_cell.rb
@@ -3,7 +3,7 @@ module Bbs
     include Cell::Hamlit
     include ActionView::Helpers::UrlHelper
 
-    delegate :bbs, :main_app, to: :parent_controller
+    delegate :bbs, :main_app, :current_user, to: :parent_controller
 
     self.view_paths = [Bbs::Engine.root.join('app/cells').to_s]
   end
