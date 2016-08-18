6877199412fc4f5ba5c175f368c1624815c20663
kajisha <kajisha@gmail.com>
2016-08-18 16:01:43 +0900

    add bbs/topics#new

--- app/controllers/bbs/topics_controller.rb
+++ app/controllers/bbs/topics_controller.rb
@@ -2,14 +2,32 @@ module Bbs
   class TopicsController < ApplicationController
     before_action :set_category
 
+    def new
+      @topic = @category.topics.build
+    end
+
+    def create
+      @topic = @category.topics.build(topic_params)
+
+      if @topic.save
+        redirect_to bbs.category_topics_path(@category)
+      else
+        redirect_back fallback_location: main_app.root_path
+      end
+    end
+
     def index
-      @topics = @category.topics
+      @topics = @category.topics.order(:created_at)
     end
 
     private
 
     def set_category
       @category = Bbs::Category.find(params[:category_id])
+    end
+
+    def topic_params
+      params.require(:topic).permit(Bbs::Topic.permitted_attributes)
     end
   end
 end
