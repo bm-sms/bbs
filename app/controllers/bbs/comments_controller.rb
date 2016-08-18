332869efb4c361d3fc7b430b6fc01a07664ae0c9
kajisha <kajisha@gmail.com>
2016-08-18 18:21:45 +0900

    create comment

--- app/controllers/bbs/comments_controller.rb
+++ app/controllers/bbs/comments_controller.rb
@@ -2,14 +2,28 @@ module Bbs
   class CommentsController < ApplicationController
     before_action :set_topic
 
+    def create
+      @comment = @topic.comments.build(comment_params)
+
+      if @comment.save
+        redirect_to bbs.topic_comments_path(@topic)
+      else
+        redirect_back fallback_location: main_app.root_path
+      end
+    end
+
     def index
       @comments = @topic.comments.order(:created_at).page(params[:page]).per(5)
     end
-
     private
 
     def set_topic
       @topic = Bbs::Topic.find(params[:topic_id])
+    end
+
+    def comment_params
+      params.require(:comment).permit(Bbs::Comment.permitted_attributes)
+        .merge(author: current_user)
     end
   end
 end
