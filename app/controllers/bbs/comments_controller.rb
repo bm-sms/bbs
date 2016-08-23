c5c744fee460ca96b3416e223629b2faaab84841
kajisha <kajisha@gmail.com>
2016-08-23 22:46:42 +0900

    add css classes and translations

--- app/controllers/bbs/comments_controller.rb
+++ app/controllers/bbs/comments_controller.rb
@@ -6,15 +6,18 @@ module Bbs
       @comment = @topic.comments.build(comment_params)
 
       if @comment.save
-        redirect_to bbs.topic_comments_path(@topic)
+        redirect_to bbs.topic_comments_path(@topic), notice: t('.created')
       else
-        redirect_back fallback_location: main_app.root_path
+        @comments = comments
+        render :index
       end
     end
 
     def index
-      @comments = @topic.comments.order(:created_at).page(params[:page]).per(5)
+      @comment = @topic.comments.build
+      @comments = comments
     end
+
     private
 
     def set_topic
@@ -24,6 +27,10 @@ module Bbs
     def comment_params
       params.require(:comment).permit(Bbs::Comment.permitted_attributes)
         .merge(author: current_user)
+    end
+
+    def comments
+      @topic.comments.order(:updated_at).page(params[:page]).per(10)
     end
   end
 end
