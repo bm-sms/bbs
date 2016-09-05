8865458aa039801bda3fba16443d8f1e0badb9c2
kajisha <kajisha@gmail.com>
2016-09-05 19:10:18 +0900

    add bbs extensions path

--- lib/bbs/engine.rb
+++ lib/bbs/engine.rb
@@ -6,5 +6,11 @@ require 'rails-i18n'
 module Bbs
   class Engine < ::Rails::Engine
     isolate_namespace Bbs
+
+    config.to_prepare do
+      Dir.glob(Rails.root + 'app/engines/**/*.rb').each do |c|
+        require_dependency(c)
+      end
+    end
   end
 end
