520fffafdd7e9278199a6463d07ba8ad2a99f8f5
kajisha <kajisha@gmail.com>
2016-09-12 15:45:07 +0900

    add `nickname` method

--- app/cells/bbs/article_cell.rb
+++ app/cells/bbs/article_cell.rb
@@ -8,6 +8,10 @@ module Bbs
       render
     end
 
+    def nickname
+      current_user.profile&.nickname || t('.noname')
+    end
+
     def avatar_url
       current_user.profile&.avatar_url || 'http://ruminate.branhamroys.com/wp-content/uploads/2015/07/unknown.gif'
     end
