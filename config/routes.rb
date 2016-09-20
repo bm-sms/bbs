b4290a87e68e27847a7dd22c66523ccc217f4701
kajisha <kajisha@gmail.com>
2016-09-20 14:30:51 +0900

    add routing for profiles resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Bbs::Engine.routes.draw do
+  resources :profiles, only: %i(edit update)
+
   resources :categories do
     resources :topics, shallow: true do
       resources :comments, shallow: true
