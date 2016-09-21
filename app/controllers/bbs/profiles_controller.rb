9c759eebe5fa8f9209ee821810f2c752182a1d92
kajisha <kajisha@gmail.com>
2016-09-21 16:26:23 +0900

    add bbs/profile#update

--- app/controllers/bbs/profiles_controller.rb
+++ app/controllers/bbs/profiles_controller.rb
@@ -4,6 +4,17 @@ module Bbs
     end
 
     def update
+      if current_user.profile.update(profile_params)
+        redirect_to action: :edit
+      else
+        render :edit
+      end
+    end
+
+    private
+
+    def profile_params
+      params.require(:user_profile).permit(Bbs::UserProfile.permitted_attributes)
     end
   end
 end
