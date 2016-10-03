be00cc5c5abae8dd704e22b60bf569e906dd8965
kajisha <kajisha@gmail.com>
2016-10-03 14:50:54 +0900

    add attachment and relation to Bbs::UserProfile

--- app/models/bbs/avatar.rb
+++ app/models/bbs/avatar.rb
@@ -1,7 +1,9 @@
 module Bbs
   class Avatar < ApplicationRecord
-    has_attached_file :avatar, styles: {medium: '48x48'}
+    has_many :user_profiles, class_name: 'Bbs::UserProfile'
 
-    validates_attachment :avatar, content_type: {content_type: %w('image/jpg' 'image/jpeg' 'image/png' 'image/gif')}
+    has_attached_file :avatar, styles: {medium: '48x48>', thumb: '48x48>'}
+
+    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   end
 end
