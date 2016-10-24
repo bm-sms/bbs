7cd9b1a1b5b5c47e38a7066b974dbdf88f8626b2
kajisha <kajisha@gmail.com>
2016-10-24 20:58:00 +0900

    remove cells generator

--- lib/generators/bbs/views_generator.rb
+++ lib/generators/bbs/views_generator.rb
@@ -2,9 +2,7 @@ require 'rails/generators/base'
 
 module Bbs
   module Generators
-    class ResourcesViewsGenerator < Rails::Generators::Base
-      hide!
-
+    class ViewsGenerator < Rails::Generators::Base
       source_root Bbs::Engine.root.join('app/views/bbs').to_s
 
       def copy_views
@@ -12,23 +10,6 @@ module Bbs
           directory resource, "app/views/bbs/#{resource}"
         end
       end
-    end
-
-    class CellsViewsGenerator < Rails::Generators::Base
-      hide!
-
-      source_root Bbs::Engine.root.join('app/cells/bbs').to_s
-
-      def copy_views
-        %w(article category latest_topics).each do |cell|
-          directory cell, "app/cells/bbs/#{cell}"
-        end
-      end
-    end
-
-    class ViewsGenerator < Rails::Generators::Base
-      invoke Bbs::Generators::ResourcesViewsGenerator
-      invoke Bbs::Generators::CellsViewsGenerator
     end
   end
 end
