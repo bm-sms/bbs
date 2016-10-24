d6226d34b008b3a4a173bafc4aef917175bdfbdf
kajisha <kajisha@gmail.com>
2016-10-24 21:39:45 +0900

    generate initializer

--- lib/generators/bbs/config_generator.rb
+++ lib/generators/bbs/config_generator.rb
@@ -2,7 +2,20 @@ require 'rails/generators/base'
 
 module Bbs
   module Generators
-    class ConfigGenerator < Rails::Generators::Base
+    class InitializerGenerator < Rails::Generators::Base
+      hide!
+
+      source_root File.expand_path('../templates', __FILE__)
+
+      def copy_initialier
+        binding.pry
+        copy_file 'bbs.rb', 'config/initializers/bbs.rb'
+      end
+    end
+
+    class LocaleGenerator < Rails::Generators::Base
+      hide!
+
       source_root Bbs::Engine.root.join('config/locales').to_s
 
       def copy_locales
@@ -10,6 +23,11 @@ module Bbs
           copy_file locale, "config/locales/#{locale}"
         end
       end
+    end
+
+    class ConfigGenerator < Rails::Generators::Base
+      invoke InitializerGenerator
+      invoke LocaleGenerator
     end
   end
 end
