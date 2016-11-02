d67c3ed224e33dd9c5482dd8af85b9640075da33
kajisha <kajisha@gmail.com>
2016-11-03 00:01:36 +0900

    add test

--- test/cells/bbs/article_cell_test.rb
+++ test/cells/bbs/article_cell_test.rb
@@ -8,7 +8,29 @@ class Bbs::ArticleCellTest < Cell::TestCase
 
   controller Bbs::ApplicationController
 
-  test 'show topic' do
+  test 'show a topic when user is not logged in' do
+    topic = Bbs::Topic.first
+
+    target = cell('bbs/article', topic).(:show)
+
+    target.find('.bbs-article__title').must_have_text(topic.title)
+    target.find('.bbs-article__body').must_have_text(topic.body.truncate_words(1))
+    target.find('.bbs-article__author-name').must_have_text(topic.author.profile.nickname)
+  end
+
+  test 'show a comment when user is not logged in' do
+    comment = Bbs::Comment.first
+
+    target = cell('bbs/article', comment).(:show)
+
+    target.find('.bbs-article__title').must_have_text(comment.title)
+    target.find('.bbs-article__body').must_have_text(comment.body.truncate_words(1))
+    target.find('.bbs-article__author-name').must_have_text(comment.author.profile.nickname)
+  end
+
+  test 'show a topic when user is logged in' do
+    Bbs::ApplicationController.current_user = Bbs::User.first
+
     topic = Bbs::Topic.first
 
     target = cell('bbs/article', topic).(:show)
@@ -18,13 +40,7 @@ class Bbs::ArticleCellTest < Cell::TestCase
     target.find('.bbs-article__author-name').must_have_text(topic.author.profile.nickname)
   end
 
-  test 'show comment' do
-    comment = Bbs::Comment.first
-
-    target = cell('bbs/article', comment).(:show)
-
-    target.find('.bbs-article__title').must_have_text(comment.title)
-    target.find('.bbs-article__body').must_have_text(comment.body)
-    target.find('.bbs-article__author-name').must_have_text(comment.author.profile.nickname)
+  teardown do
+    Bbs::ApplicationController.current_user = nil
   end
 end
