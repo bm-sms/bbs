b25f5bb4ca666d044f28631ef13564fd0a3da370
kajisha <kajisha@gmail.com>
2016-11-03 00:00:39 +0900

    authentication for test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -34,9 +34,21 @@ ActionDispatch::IntegrationTest.class_eval do
 end
 
 Bbs::ApplicationController.class_eval do
-  def current_user
-    Bbs::User.first
+  class_attribute :current_user
+
+  def current_user; self.class.current_user end
+
+  def authenticate_user_for_test
+    return if current_user
+
+    redirect_to Bbs.config.login_path
   end
+
+  helper_method :current_user
+end
+
+Bbs.configure do |config|
+  config.authenticate_user = :authenticate_user_for_test
 end
 
 # save to CircleCI's artifacts directory if we're on CircleCI
