35b08ef032eeedffa99323932485e6627c8c3b42
kajisha <kajisha@gmail.com>
2016-11-04 18:27:48 +0900

    add test for create user profile

--- test/integration/profile_test.rb
+++ test/integration/profile_test.rb
@@ -6,11 +6,9 @@ class ProfileTest < ActionDispatch::IntegrationTest
 
   fixtures 'bbs/users', 'bbs/user_profiles'
 
-  setup do
-    Bbs::ApplicationController.current_user = Bbs::User.first
-  end
-
   test 'update user profile' do
+    Bbs::ApplicationController.current_user = Bbs::User.first
+
     visit edit_profile_path
 
     choose "user_profile_avatar_id_#{Bbs::Avatar.find_by(image_file_name: 'avatar2.jpg').id}"
@@ -20,6 +18,20 @@ class ProfileTest < ActionDispatch::IntegrationTest
 
     assert_equal 'New Nickname', Bbs::User.first.profile.nickname
     assert_equal Bbs::Avatar.find_by(image_file_name: 'avatar2.jpg'), Bbs::User.first.profile.avatar
+  end
+
+  test 'create new user profile' do
+    Bbs::ApplicationController.current_user = bbs_users(:charlie)
+
+    visit edit_profile_path
+
+    choose "user_profile_avatar_id_#{Bbs::Avatar.find_by(image_file_name: 'avatar2.jpg').id}"
+    fill_in 'Nickname', with: 'New Nickname'
+
+    click_button 'Create User profile'
+
+    assert_equal 'New Nickname', bbs_users(:charlie).profile.nickname
+    assert_equal Bbs::Avatar.find_by(image_file_name: 'avatar2.jpg'), bbs_users(:charlie).profile.avatar
   end
 
   teardown do
