a31d9dad3058391bd37a19f8a485792430e89acc
kajisha <kajisha@gmail.com>
2017-01-26 19:11:14 +0900

    add user/profile/avatar configuration accessors

--- lib/bbs/configuration.rb
+++ lib/bbs/configuration.rb
@@ -2,7 +2,7 @@ module Bbs
   class Configuration
     attr_accessor :clamp_article_body, :login_path, :latest_topics_count,
                   :topics_per_page, :comments_per_page, :current_user,
-                  :authenticate_user
+                  :authenticate_user, :user_class, :user_profile_class, :avatar_class
 
     def clamp_article_body
       @clamp_article_body ||= 100
@@ -23,6 +23,15 @@ module Bbs
     def comments_per_page
       @comments_per_page ||= 10
     end
+
+    def user_class; @user_class.constantize end
+    def user_class_name; @user_class end
+
+    def user_profile_class; @user_profile_class.constantize end
+    def user_profile_class_name; @user_profile_class end
+
+    def avatar_class; @avatar_class.constantize end
+    def avatar_class_name; @avatar_class end
 
     def current_user
       @current_user ||= :current_user
