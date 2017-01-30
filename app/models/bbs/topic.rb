48beb0aa6f79fe205abceb0658ff7162c694185c
kajisha <kajisha@gmail.com>
2017-01-30 18:51:21 +0900

    use configuration

--- app/models/bbs/topic.rb
+++ app/models/bbs/topic.rb
@@ -3,7 +3,7 @@ module Bbs
     has_many :comments, class_name: 'Bbs::Comment', dependent: :delete_all
 
     belongs_to :category, class_name: 'Bbs::Category'
-    belongs_to :author, class_name: 'Bbs::User'
+    belongs_to :author, class_name: "::#{Bbs.config.user_class_name}"
 
     validates :title, :body, presence: true, allow_blank: false
   end
