d3a06de513bb2bc88f66d00bab3ac3435d8274f3
kajisha <kajisha@gmail.com>
2017-01-31 17:12:41 +0900

    'rails g bbs:config' and 'rails g active_record:bbs User'

---
+++ test/dummy/config/initializers/bbs.rb
@@ -0,0 +1,35 @@
+Bbs.configure do |config|
+  # clamp line specified words.
+  # for example, if you set `config.clamp_article_body` to `10`,
+  # comment or topics body will clamp 10 words when user does not logged in.
+  #
+  # If you set value `false`, never clamp line.
+  #config.clamp_article_body = 10
+
+  # spedify sign in path
+  #config.login_path = '/users/sign_in'
+
+  # set number of latest topics inside component
+  #config.latest_topics_count = 10
+
+  # set number of topics per page
+  #config.topics_per_page = 10
+
+  # set number of topics per page
+  #config.comments_per_page = 10
+
+  # required: set user class name
+  config.user_class = 'User'
+
+  # required: set user profile class name
+  config.user_profile_class = 'UserProfile'
+
+  # required: set avatar class name
+  config.avatar_class = 'Avatar'
+
+  # set current_user method
+  config.current_user = :current_user
+
+  # set authenticate user method
+  config.authenticate_user = :authenticate_user!
+end
