8097b169e99820cdec85e575eeeb63cd6d155531
kajisha <kajisha@gmail.com>
2017-02-20 10:35:05 +0900

    remove `.to_s`

--- app/controllers/bbs/concerns/authenticatable.rb
+++ app/controllers/bbs/concerns/authenticatable.rb
@@ -5,13 +5,13 @@ module Bbs
 
       included do
         define_method(:current_bbs_user) do
-          raise NoMethodError, "undefined method #{Bbs.config.current_user} in #{self.class.to_s}." unless respond_to?(Bbs.config.current_user)
+          raise NoMethodError, "undefined method #{Bbs.config.current_user} in #{self.class}." unless respond_to?(Bbs.config.current_user)
 
           __send__ Bbs.config.current_user
         end
 
         define_method(:authenticate_bbs_user) do
-          raise NoMethodError, "undefined method #{Bbs.config.authenticate_user} in #{self.class.to_s}." unless respond_to?(Bbs.config.authenticate_user)
+          raise NoMethodError, "undefined method #{Bbs.config.authenticate_user} in #{self.class}." unless respond_to?(Bbs.config.authenticate_user)
 
           __send__ Bbs.config.authenticate_user
         end
